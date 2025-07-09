c9785ff7305fc82c814d1f6642473b50a0955371
Bharath-sketch <bharath2005snpatty@gmail.com>
2025-07-09 22:43:27 +0530

    Update Commmand2.java

--- Commmand2.java
+++ Commmand2.java
@@ -1,10 +1,9 @@
 package basicprogram;
-
 public class Commmand2 {
 	public static void main (String args[]){
 		String str1 = args[0];
 		String str2 = args[1];
 		System.out.println(str1 +" Technologies "+str2);
 	}
-
 }
+//Write a Program that accepts two Strings as command line arguments and generate the output in the required format. 

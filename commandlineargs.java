8ef51af2d3cd4af3ec717cb802f971e737b4442b
Bharath-sketch <bharath2005snpatty@gmail.com>
2025-07-09 22:40:08 +0530

    Update commandlineargs.java

--- commandlineargs.java
+++ commandlineargs.java
@@ -5,3 +5,4 @@ public class commandlineargs {
 		System.out.println("Welcome "+str1);
 		}
 }
+//2.Write a Program to accept a String as a command line argument and print a Welcome message as given below. 

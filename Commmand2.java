8b36efa1a8e2815e7d5904e200efab9f455be3d5
bharath <bharath@LAPTOP-7O0UHJT4>
2025-07-09 21:32:37 +0530

    first commit

---
+++ Commmand2.java
@@ -0,0 +1,10 @@
+package basicprogram;
+
+public class Commmand2 {
+	public static void main (String args[]){
+		String str1 = args[0];
+		String str2 = args[1];
+		System.out.println(str1 +" Technologies "+str2);
+	}
+
+}
---
+++ Welcome.java
@@ -0,0 +1,9 @@
+package basicprogram;
+
+public class Welcome {
+	public static void main(String args[]) {
+		String str1 =args[0];
+		System.out.println("Welcome "+str1);
+	}
+
+}
